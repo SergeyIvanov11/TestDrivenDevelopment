a58cbf2d5fd763b7616d03f4f088315085581f70
Sergey Ivanov <1539ivanov@mail.ru>
2024-04-28 00:17:39 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -2,8 +2,9 @@ package org.example;
 
 public class PhoneBook {
 
-
-
+    public int add(String name, String phoneNumber) {
+        return 0;
+    }
 
 
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,7 +1,28 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    void testAdd1() {
+        int firstTest = phoneBook.add("Petya", "+79161234567");
+        Assertions.assertEquals(1, firstTest);
+    }
+
+    @Test
+    void testAdd2() {
+        int secondTest = phoneBook.add("Fedya", "+79161234568");
+        Assertions.assertEquals(2, secondTest);
+    }
+
+    @Test
+    void testAdd3() {
+        int thirdTest = phoneBook.add("Petya", "+79161234569");
+        Assertions.assertEquals(2, thirdTest);
+    }
+
 
 }
